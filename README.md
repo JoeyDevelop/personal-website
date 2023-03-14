5feec1434ca5348aefcde288f7ab9cf5876acadb
Joey Patterson <joeypatterson177@gmail.com>
2023-03-14 00:44:33 -0400

    create projects and skills sections

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Welcome to my Personal Website. This website will be used for applications and to show off my skills.
 
+!!(note CV App github pages is currently broken and needs to be fixed and added to site)
+
 Check me out!
--- index.js
+++ index.js
@@ -15,3 +15,20 @@ function showDivs(n) {
   }
   x[slideIndex-1].style.display = "block";
 }
+
+showProjectDivs(slideIndex);
+
+function plusProjectDivs(n) {
+  showProjectDivs(slideIndex += n);
+}
+
+function showProjectDivs(n) {
+  let i;
+  let x = document.getElementsByClassName("myProjectSlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "flex";
+}
